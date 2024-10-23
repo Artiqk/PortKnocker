7a2ad73cfd7da6022e6ace9a8a10002d9e3b0a42
Antoine A. <antoineabonneau94@gmail.com>
2024-10-23 17:28:42 +0200

    Added typing + docstring

--- app/network_utils.py
+++ app/network_utils.py
@@ -1,7 +1,9 @@
 import psutil
 import socket
+from typing import List
 
-def get_local_ips(exclude_list=['127.0.0.1']):
+def get_local_ips(exclude_list: List[str] = ['127.0.0.1']) -> List[str]:
+    """Retrieve a list of local IP addresses, excluding those in the exclude list."""
     local_ips = []
     for interface, addrs in psutil.net_if_addrs().items():
         local_ips.extend([addr.address for addr in addrs if addr.family == socket.AF_INET and addr.address not in exclude_list])
