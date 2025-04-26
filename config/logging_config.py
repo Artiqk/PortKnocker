7f82db480c5ce8799db29130a6bfb03804ae2296
Antoine A. <antoineabonneau94@gmail.com>
2025-04-26 02:39:43 +0200

    v3

--- config/logging_config.py
+++ config/logging_config.py
@@ -4,11 +4,11 @@ import sys
 def setup_logging(config):
     handlers = [logging.StreamHandler(sys.stderr)]
 
-    if config.DEBUG:
+    if config == 'DEBUG':
         handlers.append(logging.FileHandler('./logs/app.log', mode='a'))
 
     logging.basicConfig(
-        level=logging.DEBUG if config.DEBUG else logging.INFO,
+        level=logging.DEBUG if config == 'DEBUG' else logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=handlers
     )
